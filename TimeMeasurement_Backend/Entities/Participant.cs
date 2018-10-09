9463911a863f70b479d85c79c2f967abdd685630
Peter Hauer <pethaudi@yahoo.de>
2018-10-09 13:35:40 +0200

    added all regex, checked via regexr.com

--- TimeMeasurement_Backend/Entities/Participant.cs
+++ TimeMeasurement_Backend/Entities/Participant.cs
@@ -46,16 +46,30 @@ namespace TimeMeasurement_Backend.Entities
         [Required]
         [Range(1919, 3000)]
         public int YearGroup { get; set; }
+
         [Required]
+        [RegularExpression(@"^[A-Z][a-z0-9-:/]*$")]
         public string PostalCode { get; set; }
+
         [Required]
+        [RegularExpression(@"^([A-Za-z ./-])*$")]
         public string City { get; set; }
+
         [Required]
+        [RegularExpression(@"^([A - Za - z./ -])*$")]
+        public string Street { get; set; }
+
+        [Required]
+        [RegularExpression(@"^([0-9a-zA-Z/])*$")]
         public string HouseNumber { get; set; }
+
         public string Team { get; set; }
+
         [Required]
-        [RegularExpression("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01 -\x08\x0b\x0c\x0e -\x1f\x21\x23 -\x5b\x5d -\x7f] |\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])")]
+        [RegularExpression("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01 -\x08\x0b\x0c\x0e -\x1f\x21\x23 -\x5b\x5d -\x7f] |\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])")]
         public string Email { get; set; }
+
+        [RegularExpression("^(?:(?:\\(?(?:00|\\+)([1-4]\\d\\d|[1-9]\\d?)\\)?)?[\\-\\.\\ \\\\\\/]?)?((?:\\(?\\d{1,}\\)?[\\-\\.\\ \\\\\\/]?){0,})(?:[\\-\\.\\ \\\\\\/]?(?:#|ext\\.?|extension|x)[\\-\\.\\ \\\\\\/]?(\\d+))?$")]
         public string PhoneNumber { get; set; }
     }
 }
