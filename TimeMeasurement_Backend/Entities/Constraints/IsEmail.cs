46c603d7dbb1a29e2f3a528650c559bffae2409e
Elias Bürger <elias.buerger@gmx.at>
2018-10-09 15:14:54 +0200

    Added Country code validation and participant id

---
+++ TimeMeasurement_Backend/Entities/Constraints/IsEmail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
+
+namespace TimeMeasurement_Backend.Entities.Constraints
+{
+    /// <summary>
+    /// Attribute to check if string is an email
+    /// </summary>
+    public class IsEmail : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            //Email must not be null
+            if (value == null)
+            {
+                return new ValidationResult("Input was null");
+            }
+
+            try
+            {
+                //Try to convert string to mailaddress
+                var mailAddress = new MailAddress((string)value);
+                return ValidationResult.Success;
+            }
+            catch (Exception ex)
+            {
+                //Exeception, when conversion is not possible
+                return new ValidationResult(ex.Message);
+            }
+        }
+    }
+}
