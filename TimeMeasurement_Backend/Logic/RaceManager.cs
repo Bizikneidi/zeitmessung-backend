38c0caa3a9570652f43cfa69f6c7664d56711ae2
Peter Hauer <pethaudi@yahoo.de>
2018-11-13 14:24:42 +0100

    added standard methodes from the old time meter

--- TimeMeasurement_Backend/Logic/RaceManager.cs
+++ TimeMeasurement_Backend/Logic/RaceManager.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace TimeMeasurement_Backend.Logic
 {
     public class RaceManager
@@ -12,5 +14,73 @@ namespace TimeMeasurement_Backend.Logic
             InProgress, //The race is in progress
             Disabled //The time meter can not start a measurement and nobody can request one
         }
+
+        private State _currentState;
+        
+        /// <summary>
+        /// Event to allow others to act accoring to the current state of the time meter
+        /// </summary>
+        public event Action<State, State> StateChanged;
+        
+        /// <summary>
+        /// The current state of the time meter
+        /// </summary>
+        public State CurrentState
+        {
+            get => _currentState;
+            private set
+            {
+                var prev = _currentState;
+                _currentState = value;
+                StateChanged?.Invoke(prev, _currentState); //Notify subscribers to act
+            }
+        }
+        
+        private RaceManager() => _currentState = State.Disabled;
+        
+        /// <summary>
+        /// Allows others to set the time meter to disabled
+        /// </summary>
+        public void Disable()
+        {
+            CurrentState = State.Disabled;
+        }
+
+        /// <summary>
+        /// Allows others to set the time meter to ready
+        /// </summary>
+        public void Ready()
+        {
+            //Only possible, if time meter is disabled
+            if (CurrentState == State.Disabled)
+            {
+                CurrentState = State.Ready;
+            }
+        }
+
+        /// <summary>
+        /// Allows others to set the time meter to StartRequested
+        /// </summary>
+        public void RequestStart()
+        {
+            if (CurrentState == State.Ready)
+            {
+                CurrentState = State.StartRequested;
+            }
+        }
+
+        /// <summary>
+        /// Allows others to set the time meter to InProgress
+        /// </summary>
+        public void Start(long startTime)
+        {
+            if (CurrentState == State.StartRequested)
+            {
+                //Pass to TimeMeter
+                CurrentState = State.InProgress;
+            }
+        }
+        
+        
     }
 }
